90c66ed7b94e6356618440a3849adb757699059a
Maciej Pasternacki <maciej@pasternacki.net>
2013-02-01 21:11:00 +0100

    New subcommand: vendor diff

--- lib/vendorificator/cli.rb
+++ lib/vendorificator/cli.rb
@@ -6,7 +6,7 @@ module Vendorificator
   class CLI < Thor
     include Vendorificator
 
-    check_unknown_options!
+    check_unknown_options! :except => [:diff]
     default_task :sync
 
     class_option :file, :aliases => '-f', :type => :string, :banner => 'PATH'
@@ -14,11 +14,21 @@ module Vendorificator
 
     MODULES_USAGE     = '[module [module [module ...]]]'
     MODULES_DESC      = 'If no modules are given, all modules will be used.'
+    GIT_OPTIONS_USAGE = '[--git-options --opt1 [--opt2 [--opt3 ...]]]'
+    GIT_OPTIONS_DESC  = "All options passed after '--git-options' will be passed directly to Git."
+
     def initialize(*args)
       super
       Grit.debug = true if options[:debug]
       Vendorificator::Config.from_file(find_vendorfile)
       Vendorificator::Config[:shell] = shell
+
+      class << shell
+        # Make say_status always say it.
+        def quiet?
+          false
+        end
+      end
     end
 
     desc :sync, "Download new or updated vendor files"
@@ -59,6 +69,33 @@ EOF
       end
     end
 
+    desc "diff #{MODULES_USAGE} #{GIT_OPTIONS_USAGE}",
+         "Show differences between work tree and upstream module(s)"
+    long_desc <<EOF
+Shows differences between work tree and upstream module(s) by calling `git diff`.
+#{MODULES_DESC}
+#{GIT_OPTIONS_DESC}
+EOF
+    method_option :quiet, :aliases => ['-q'], :default => false, :type => :boolean
+    method_option :only_changed, :default => false, :type => :boolean
+    def diff(*args)
+      modules, git_options = split_git_options(args)
+      Vendorificator::Config.each_module(*modules) do |mod|
+        unless mod.merged
+          say_status 'unmerged', mod.to_s, :red unless options[:only_changed]
+          next
+        end
+        git_args = git_options + [ mod.merged, '--', mod.work_dir ]
+        diff = repo.git.native('diff', {}, *git_args)
+        if diff.empty?
+          say_status 'unchanged', mod.to_s, :green unless options[:only_changed]
+        else
+          say_status 'changed', mod.to_s, :yellow
+        end
+        puts diff unless options[:quiet] || diff.empty?
+      end
+    end
+
     desc :pull, "Pull upstream branches from a remote repository"
     method_option :remote, :aliases => ['-r'], :default => nil
     method_option :dry_run, :aliases => ['-n'], :default => false, :type => :boolean
@@ -95,6 +132,14 @@ EOF
     end
 
     private
+
+    def split_git_options(args)
+      case i = args.index('--git-options')
+      when nil then [ args, [] ]
+      when 0 then [ [], args[1..-1] ]
+      else [ args[0..(i-1)], args[(i+1)..-1] ]
+      end
+    end
 
     def conf
       Vendorificator::Config
