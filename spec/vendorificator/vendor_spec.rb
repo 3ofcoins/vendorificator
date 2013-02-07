d60dce64ec97b3ee8cd76d1f9ed0aa9e876d09a8
Maciej Pasternacki <maciej@pasternacki.net>
2013-02-07 13:09:38 +0100

    Small spec case cleanup.

--- spec/vendorificator/vendor_spec.rb
+++ spec/vendorificator/vendor_spec.rb
@@ -5,8 +5,11 @@ module Vendorificator
     @category = :test
   end
 
-  describe Vendor do
+  class Vendor::Custom < Vendor
+    @method_name = :whatever
+  end
 
+  describe Vendor do
     describe '.category' do
       it 'defaults to nil' do
         assert { Vendor.category == nil }
@@ -14,6 +17,32 @@ module Vendorificator
 
       it 'can be overridden in a subclass' do
         assert { Vendor::Categorized.category == :test }
+      end
+    end
+
+    describe '.install!' do
+      it "creates a method inside Vendorificator::Config" do
+        deny { Vendorificator::Config.respond_to?(:categorized) }
+
+        Vendor::Categorized.install!
+        assert { Vendorificator::Config.respond_to?(:categorized) }
+
+        # Clean up and make sure it has worked
+        class << Vendorificator::Config ; remove_method :categorized ; end
+        deny { Vendorificator::Config.respond_to?(:categorized) }
+      end
+
+      it "uses @method_name for method's name if set" do
+        deny { Vendorificator::Config.respond_to?(:custom) }
+        deny { Vendorificator::Config.respond_to?(:whatever) }
+
+        Vendor::Custom.install!
+        deny   { Vendorificator::Config.respond_to?(:custom) }
+        assert { Vendorificator::Config.respond_to?(:whatever) }
+
+        # Clean up and make sure it has worked
+        class << Vendorificator::Config ; remove_method :whatever ; end
+        deny { Vendorificator::Config.respond_to?(:whatever) }
       end
     end
 
