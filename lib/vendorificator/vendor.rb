549237e5296fc5c13c2b5248fafc210e3191e33c
Maciej Pasternacki <maciej@pasternacki.net>
2013-03-02 12:04:25 +0100

    Don't bomb on cleaning an empty repository.

--- lib/vendorificator/vendor.rb
+++ lib/vendorificator/vendor.rb
@@ -213,10 +213,10 @@ module Vendorificator
         # orphaned branch.
         if self.head
           environment.git.checkout branch_name
-          environment.git.rm( { :r => true, :f => true, :q => true }, '.') if options[:clean]
+          environment.git.rm( { :r => true, :f => true, :q => true, :ignore_unmatch => true }, '.') if options[:clean]
         else
           environment.git.checkout( { :orphan => true }, branch_name )
-          environment.git.rm( { :r => true, :f => true, :q => true }, '.')
+          environment.git.rm( { :r => true, :f => true, :q => true, :ignore_unmatch => true }, '.')
         end
       end
 
