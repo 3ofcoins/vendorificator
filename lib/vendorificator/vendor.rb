98c861994b531f49ba9701a138760f53521d9f1e
Maciej Pasternacki <maciej@pasternacki.net>
2013-03-08 15:33:45 +0100

    Quiet git rev-parse.

--- lib/vendorificator/vendor.rb
+++ lib/vendorificator/vendor.rb
@@ -124,13 +124,13 @@ module Vendorificator
     end
 
     def head
-      environment.git.capturing.rev_parse({:verify => true}, "refs/heads/#{branch_name}").strip
+      environment.git.capturing.rev_parse({:verify => true, :quiet => true}, "refs/heads/#{branch_name}").strip
     rescue MiniGit::GitError
       nil
     end
 
     def tagged_sha1
-      @tagged_sha1 ||= environment.git.capturing.rev_parse({:verify => true}, "refs/tags/#{tag_name}^{commit}").strip
+      @tagged_sha1 ||= environment.git.capturing.rev_parse({:verify => true, :quiet => true}, "refs/tags/#{tag_name}^{commit}").strip
     rescue MiniGit::GitError
       nil
     end
